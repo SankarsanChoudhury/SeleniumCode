d6f15cc7b80717fbc9d2bfb4e2764c8f53ba8466
SankarsanChoudhury <42717747+SankarsanChoudhury@users.noreply.github.com>
2020-03-14 23:48:29 +0530

    Added Add() method which will add 2 integers
    Added a static method Add() which will add 2 integers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld
+  
+  static int Add(int a,int b)
+{
+  int c=a+b;
+  return c;
+}
 
 public static void main(String args[]){
 System.out.println("Hello World");
+  
+  HelloWorld.Add();
 }
